02c081e005989a24fe0f2ec491972dd1960bdb45
abbos-21 <abbos.nazariy0621@gmail.com>
2025-12-21 08:45:56 +0500

    commit 128

--- src/routes/stars.ts
+++ src/routes/stars.ts
@@ -4,6 +4,20 @@ import { bot } from "../bot";
 
 const router = express.Router();
 
-router.post("/create-invoice", async (req: Request, res: Response) => {});
+router.post("/create-invoice", async (req: Request, res: Response) => {
+  const invoiceLink = await bot.api.createInvoiceLink(
+    "Title", //title
+    "Some description", //description
+    "{}", //payload
+    "", // For Telegram Stars payment this should be empty
+    "XTR", //currency
+    [{ amount: 1, label: "Diamond" }]
+  );
+
+  res.status(200).json({
+    success: true,
+    data: { invoiceLink },
+  });
+});
 
 export default router;
