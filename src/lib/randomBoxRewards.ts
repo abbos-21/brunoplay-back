677d92e159fc347b7a601975640b9f754304608d
abbos-21 <abbos.nazariy0621@gmail.com>
2025-12-24 23:31:55 +0500

    commit 139

--- src/lib/randomBoxRewards.ts
+++ src/lib/randomBoxRewards.ts
@@ -1,17 +1,17 @@
 export const rewards = [
-  { id: 1, name: "Energy Refill", chance: 45.0 },
-  { id: 2, name: "Health Refill", chance: 45.0 },
-  { id: 3, name: "3000 Coins", chance: 8.0 },
+  { id: 1, name: "Energy Refill", chance: 49.9 },
+  { id: 2, name: "Health Refill", chance: 49.9 },
+  { id: 3, name: "3000 Coins", chance: 0.0567 },
 
-  { id: 4, name: "5000 Coins", chance: 0.424444 },
-  { id: 5, name: "7000 Coins", chance: 0.373333 },
-  { id: 6, name: "10000 Coins", chance: 0.322222 },
-  { id: 7, name: "20000 Coins", chance: 0.271111 },
-  { id: 8, name: "50000 Coins", chance: 0.22 },
-  { id: 9, name: "75000 Coins", chance: 0.168889 },
-  { id: 10, name: "100000 Coins", chance: 0.117778 },
-  { id: 11, name: "100 Stars", chance: 0.066667 },
-  { id: 12, name: "Gift", chance: 0.035556 },
+  { id: 4, name: "5000 Coins", chance: 0.0234 },
+  { id: 5, name: "7000 Coins", chance: 0.0122 },
+  { id: 6, name: "10000 Coins", chance: 0.0067 },
+  { id: 7, name: "20000 Coins", chance: 0.004 },
+  { id: 8, name: "50000 Coins", chance: 0.003 },
+  { id: 9, name: "75000 Coins", chance: 0.0015 },
+  { id: 10, name: "100000 Coins", chance: 0.0015 },
+  { id: 11, name: "100 Stars", chance: 0.001 },
+  { id: 12, name: "Gift", chance: 0.001 },
 ];
 
 export const rewardById = new Map(rewards.map((r) => [r.id, r]));
