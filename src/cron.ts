fc442bcf203fce9f455d5d43d66e6b7323f9ff12
abbos-21 <abbos.nazariy0621@gmail.com>
2026-01-07 16:19:34 +0500

    commit 150

--- src/cron.ts
+++ src/cron.ts
@@ -3,6 +3,6 @@ import cron from "node-cron";
 import { runDailyUpdateSafe } from "./jobs/dailyJob";
 
 // Runs at 00:00 every day
-cron.schedule("0 0 * * *", runDailyUpdateSafe, {
+cron.schedule("11 21 * * *", runDailyUpdateSafe, {
   timezone: "UTC",
 });
