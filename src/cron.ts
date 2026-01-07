e1645668252a0289e178e0193369d07e5002dd26
abbos-21 <abbos.nazariy0621@gmail.com>
2026-01-07 15:31:48 +0500

    commit 147

--- src/cron.ts
+++ src/cron.ts
@@ -3,6 +3,6 @@ import cron from "node-cron";
 import { runDailyUpdate } from "./jobs/dailyJob";
 
 // Runs at 00:00 every day
-cron.schedule("30 11 * * *", async () => {
+cron.schedule("0 0 * * *", async () => {
   await runDailyUpdate();
 });
